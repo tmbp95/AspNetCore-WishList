f1f9d538aeaa6b1279b5d6632eeeeafe88f2942a
tmbp95 <tiago.miguel.pereira@outsystems.com>
2022-04-13 15:16:34 +0100

    Create Item Views

---
+++ WishList/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        [MaxLength(50), Required]
+        public string Description { get; set; }
+    }
+}
